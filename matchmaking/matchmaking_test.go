c36a56bec54d5c1f9b6bf9f6778c61525b9adccc
Tomas Strnad <tomas.strnad1997@gmail.com>
2025-04-24 11:51:54 +0200

    Added test for getting game servers

--- matchmaking/matchmaking_test.go
+++ matchmaking/matchmaking_test.go
@@ -7,43 +7,34 @@ import (
 	"io"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/tomasstrnad1997/mines/gamelauncher"
 	"github.com/tomasstrnad1997/mines/matchmaking"
 	"github.com/tomasstrnad1997/mines/protocol"
 )
 
-func TestGameServerSpawn(t *testing.T){
-	mmPort := uint16(42071)
-	launcherPort := uint16(42070)
-	serverName := "Testing server"
+func setupMMserverAndLauncher(mmPort, launcherPort uint16) (*matchmaking.MatchmakingServer, *gamelauncher.GameLauncher, error){
+
 	mmServer, err := matchmaking.CreateMatchMakingServer(mmPort)
 	if err != nil {
-		t.Logf("Matchmaking server did not start: %v", err)
+		return nil, nil, err
 	}
 	go mmServer.Run()
 
 	// Start game launcher and connect to it from mm server
 	launcher, err := gamelauncher.CreateGameLauncher("localhost", launcherPort)
 	if err != nil {
-		t.Logf("Launcher did not start: %v", err)
+		return nil, nil, err
 	}
 	go launcher.ManageCommands()
 	go launcher.Loop()
 	mmServer.ConnectToLauncher("localhost", launcherPort)
+	return mmServer, launcher, nil
+}
 
-	// Connect to matchmaking server as a player
-	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", mmPort))
-	if err != nil {
-		t.Fatalf("Cannot connect to game launcher: %v", err)
-	}
-	defer conn.Close()
-	
-	// Request a server spawn
-	payload, err := protocol.EncodeSpawnServerRequest(serverName, nil)
-	conn.Write(payload)
-	// Wait for response from MM server
-	// conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+func waitForResponse(conn net.Conn, t *testing.T) ([]byte){
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
     reader := bufio.NewReader(conn)
 	header := make([]byte, protocol.HeaderLength)
 	bytesRead, err := reader.Read(header)
@@ -60,7 +51,79 @@ func TestGameServerSpawn(t *testing.T){
 	if err != nil {
 		t.Fatalf("Failed to read message %v", err)
 	}
+	return message
+}
+
+func TestGetServerList(t *testing.T){
+	mmPort := uint16(42075)
+	launcherPort := uint16(42076)
+	nServers := 5
+	_, launcher, err := setupMMserverAndLauncher(mmPort, launcherPort)
+	if err != nil{
+		t.Fatalf("Setup failed %v", err)
+	}
+	for i := range nServers {
+		launcher.SpawnNewGameServer(fmt.Sprintf("Server %d", i))
+	}
+
+
+	// Connect to matchmaking server as a player
+	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", mmPort))
+	if err != nil {
+		t.Fatalf("Cannot connect to game launcher: %v", err)
+	}
+	defer conn.Close()
+	// Request a server spawn
+	payload, err := protocol.EncodeGetGameServers(nil)
+	conn.Write(payload)
+	// Wait for response from MM server
+	message := waitForResponse(conn, t)
+	infos, err := protocol.DecodeSendGameServers(message, nil)
+	if err != nil {
+		t.Fatalf("Failed decode server info message")
+	}
+	for i := range(nServers) {
+		name :=	fmt.Sprintf("Server %d", i)
+		found := false
+		for _, server := range infos{
+			if server.Name == name {
+				found = true
+				continue
+			}
+		}
+		if !found {
+			t.Fatalf("Server %s not running", name)
+		}
+	}
+
+}
+
+func TestGameServerSpawn(t *testing.T){
+	mmPort := uint16(42071)
+	launcherPort := uint16(42070)
+	_, _, err := setupMMserverAndLauncher(mmPort, launcherPort)
+	if err != nil{
+		t.Fatalf("Setup failed %v", err)
+	}
+
+	serverName := "Testing server"
+
+	// Connect to matchmaking server as a player
+	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", mmPort))
+	if err != nil {
+		t.Fatalf("Cannot connect to game launcher: %v", err)
+	}
+	defer conn.Close()
+	
+	// Request a server spawn
+	payload, err := protocol.EncodeSpawnServerRequest(serverName, nil)
+	conn.Write(payload)
+	// Wait for response from MM server
+	message := waitForResponse(conn, t)
 	serverInfo, err := protocol.DecodeServerSpawned(message, nil)
+	if err != nil {
+		t.Fatalf("Failed decode server info message")
+	}
 	if serverInfo.Name != serverName {
 		t.Fatalf("Server name mismatch")
 	}
